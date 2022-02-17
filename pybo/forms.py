ef69a208d0619ab6ebe78ef67d43c3e1d8c5409a
유동녕 <dongyoungyu@yahoo.com>
2022-02-17 15:32:50 +0900

    noupload forth commit

--- pybo/forms.py
+++ pybo/forms.py
@@ -7,11 +7,10 @@ from pybo.models import Question, Answer, Comment
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question  # 사용할 모델
-        fields = ['subject', 'content', 'imgfile']  # QuestionForm에서 사용할 Question 모델의 속성
+        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
         labels = {
             'subject': '제목',
             'content': '내용',
-            'imgfile': '첨부파일',
         } 
 
 class AnswerForm(forms.ModelForm):
